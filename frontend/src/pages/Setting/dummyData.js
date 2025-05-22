7ef636490f8c54756e8d2deb76c315e09bdc3c56
Sumona17 <sumona.mondal@exavalu.com>
2025-05-22 14:27:30 +0530

    Version1.0.6_DeletedUnnecessary

--- frontend/src/pages/Setting/dummyData.js
+++
@@ -1,8 +0,0 @@
-export const initialValues = {
-    name: 'Lisa Doe',
-    email: 'support@gmail.com',
-    phone: '+123 456 777',
-    website: 'www.suppoert.com',
-    password: '********',
-    confirmpassword: '********',
-};
